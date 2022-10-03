e843b1a76d5736667e76b13d65ed179dc967e321
harioct <octavianhari@gmail.com>
2022-10-03 20:28:58 +0700

    first commit

---
+++ PRAK002-2210817210033-HariOctavianDelrossi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Selamat Pagi, Hari Octavian Delrossi\n");
+    printf("Selamat Siang, Hari Octavian Delrossi\n");
+    printf("Selamat Malam, Hari Octavian Delrossi\n");
+    return 0;
+}
---
+++ PRAK004-2210817210033-HariOctavianDelrossi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#   Hari Octavian Delrossi   #\n");
+    printf("#       2210817210033        #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
